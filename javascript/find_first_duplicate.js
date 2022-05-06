46a4812cc3175aaef13664de8390e34f3ad5010a
Myles <bracken.myles@gmail.com>
2022-05-05 19:44:44 -0700

    completed

--- javascript/find_first_duplicate.js
+++ javascript/find_first_duplicate.js
@@ -1,5 +1,13 @@
 function findFirstDuplicate(arr) {
-  // type your code here
+  const uniques = new Set();
+
+  for (const value of arr) {
+    if (uniques.has(value)) {
+      return value;
+    }
+    uniques.add(value);
+  }
+  return -1;
 }
 
 if (require.main === module) {
@@ -16,4 +24,22 @@ if (require.main === module) {
 module.exports = findFirstDuplicate;
 
 // Please add your pseudocode to this file
+/******************************************************************
+ * initialize an empty set called uniques
+ *
+ * iterate through the input array:
+ *    if the value is in uniques, return the value
+ *    else add the value to the set
+ *
+ * return -1 if no duplicate found during iteration
+ * ***************************************************************/
+
 // And a written explanation of your solution
+/*******************************************************************
+ * A Set is a data structure that contains only unique objects/values.
+ * If I check if a value is in a Set before adding it, I'll know if there's
+ * a duplicate. If there's a duplicate, I'll just return that value right
+ * away because that'll be the first duplicate in the input array. If we
+ * exit iteration without returning anything, that means there's no duplicate,
+ * so we'll return -1
+ * *******************************************************************/
